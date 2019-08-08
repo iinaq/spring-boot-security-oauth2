2ee12d37a4843b64f9802669dff51bb40063e422
iinaq <302506562@qq.com>
2019-08-08 11:11:00 +0800

    commit

---
+++ security-oauth2-resources/src/main/java/com/iinaq/securityoauth2resources/SecurityOauth2ResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.iinaq.securityoauth2resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauth2ResourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOauth2ResourcesApplication.class, args);
+    }
+
+}
---
+++ security-oauth2-resources/src/test/java/com/iinaq/securityoauth2resources/SecurityOauth2ResourcesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iinaq.securityoauth2resources;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityOauth2ResourcesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
